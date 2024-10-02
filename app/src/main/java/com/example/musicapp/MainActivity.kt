66228f4ca6f2ea96955ca4e33b4648c671e9ed91
javier <javier.gastonma@elorrieta-errekamari.com>
2024-10-02 23:40:37 +0200

    FEATURE Interfaz MusicApp Compose inicio

---
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.musicapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.musicapp.ui.theme.MusicAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MusicAppTheme {
+                Orientation()
+            }
+        }
+    }
+}
+
+@Composable
+fun Orientation() {
+    val configuration = LocalConfiguration.current
+
+    if (configuration.orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE) {
+        MusicAppInterface()
+    } else {
+        MusicAppInterface()
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MusicAppTheme {
+        MusicAppInterface()
+    }
+}
